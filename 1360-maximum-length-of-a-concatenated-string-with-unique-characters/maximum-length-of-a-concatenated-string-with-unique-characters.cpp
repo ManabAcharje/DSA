151040c050dd0de850e84b271385cb56b7236d00
Manab Acharjee <manavacharje@gmail.com>
2026-03-20 23:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (91.67%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    bool check(string& s, string& t) {
-        int arr[26] = {0};
+    /// _BIT MANIPULATION TECHNIQUE_ ///
 
-        for (int i = 0; i <s.size(); i++) {
-            arr[s[i] - 'a']++;
+    int solve(vector<int>&nums, int i,int n,int num){
+        if(i==n)return __builtin_popcount(num);
+        int include=0;
+        int exclude=0;
+        if(num & nums[i]){// means duplicate occurs so don't include;
+            return solve(nums,i+1,n,num);
         }
-        // also  need to know is string t valid or not 
-        for (int i = 0; i <t.size(); i++) {
-            if (arr[t[i] - 'a'] > 0)
-                return true;
-            arr[t[i]-'a']++;
-            
-            
+        else{
+            include=solve(nums,i+1,n,num|nums[i]);
+            exclude=solve(nums,i+1,n,num);
         }
-        return false;
+        return max(include,exclude);
     }
-    int solve(vector<string>& arr, int i, int n, string s) {
-        // base case
-        if (i == n)
-            return s.length();
-
-        int pick = 0;
-        int not_pick = 0;
-
-        if (check(s, arr[i])) {// has duplicate
-            not_pick = solve(arr, i + 1, n, s);
-            pick = 0;
-        } else {
-            pick = solve(arr, i + 1, n, s + arr[i]);
-            not_pick = solve(arr, i + 1, n, s);
+    int maxLength(vector<string>& arr) {
+        vector<int>nums;
+        for(string &s:arr)
+        {
+            int num=0;
+            bool duplicate=false;
+            for(int i=0;i<s.size();i++){
+                //bit already set then continue , because the i.e duplicate so need to
+                int pos=s[i]-'a';
+                if(num & (1<<pos)){
+                    duplicate=true;
+                    break;
+                } 
+                num=num|(1<<pos);
+            }
+            if(!duplicate)nums.push_back(num);
         }
 
-        return max(not_pick, pick);
-    }
-    int maxLength(vector<string>& arr) { 
-        int n=arr.size();
-        return solve(arr, 0, n, ""); 
+        return solve(nums,0,nums.size(),0);
     }
 };
