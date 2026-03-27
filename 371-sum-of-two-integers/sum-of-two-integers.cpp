e280e6d201a8fdd1be20475d44c25faa4026abc6
Manab Acharjee <manavacharje@gmail.com>
2026-03-27 21:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (33.75%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
-        
+        int sum = 0;
+        int carry = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bit1 = (a >> i) & 1;
+            int bit2 = (b >> i) & 1;
+
+            int currentBit = bit1 ^ bit2 ^ carry;
+
+            sum |= (currentBit << i);
+            carry = (bit1 & bit2) | (bit2 & carry) | (bit1 & carry);
+        }
+
+        return sum;
     }
 };
