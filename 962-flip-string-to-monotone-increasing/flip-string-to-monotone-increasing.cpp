a74937d27463935e2e0f0a9d41fdc781bee2b408
Manab Acharjee <manavacharje@gmail.com>
2026-04-02 22:31:04 +0530

    Time: 50 ms (19.56%) | Memory: 28.2 MB (20.29%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
-    int minFlipsMonoIncr(string s) {
-        int ones = 0, flips = 0;
+    int n;
+    int dp[1000001][2];
+    int solve(int i,int prev,string &s){
+        if(i==n)return 0;
 
-        for (char c : s) {
-            if (c == '1') {
-                ones++;
-            } else {
-                flips = min(flips + 1, ones);
+        int flip=INT_MAX;
+        int not_flip=INT_MAX;
+        if(dp[i][prev]!=-1)return dp[i][prev];
+
+        if(s[i]=='0')
+        {   
+
+            if(prev==0){
+                not_flip=solve(i+1,0,s);
+                flip=1+solve(i+1,1,s);
             }
+            else{
+                //prev==1
+                flip=1+solve(i+1,1,s);
+            }
+            
+            
         }
-        return flips;
+        else if(s[i]=='1'){
+            if(prev==0){
+                not_flip=solve(i+1,1,s);
+                flip=1+solve(i+1,0,s);
+
+            }
+            else{
+                //prev=1;
+                not_flip=solve(i+1,1,s);
+
+            }
+
+        }
+        return  dp[i][prev]=min(flip,not_flip);
+    }
+    int minFlipsMonoIncr(string s) {
+        n=s.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,s);
     }
 };
