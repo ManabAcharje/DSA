537231f528c265bebd104b7aa54649ef6bbc5d12
Manab Acharjee <manavacharje@gmail.com>
2026-05-01 15:04:23 +0530

    Time: 4 ms (40.21%) | Memory: 99.5 MB (79.08%) - LeetSync

---
+++ 396-rotate-function/rotate-function.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n=nums.size();
+        int f=0;int sum=0;
+        for(int i=0;i<n;i++){
+            f+=(i*nums[i]);
+            sum+=nums[i];
+        }
+        int ans=f;
+        for(int k=1;k<n;k++){
+            f=f+sum-(nums[n-k]*n);
+            ans=max(f,ans);
+        }
+        return ans;
+    }
+};
