43c39c900f3ac5bfaff317ed413edf056b938485
Manab Acharjee <manavacharje@gmail.com>
2026-05-22 01:15:57 +0530

    Time: 507 ms (15.64%) | Memory: 345.8 MB (14.17%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>>adj;
+    int n;
     vector<bool>visited;
-    bool dfs(int source,int destination){
-        visited[source]=true;
-        if(source==destination)return true;
-        for(int ngbr:adj[source]){
-            if(ngbr==destination)return true;
-            else{
-                if(!visited[ngbr]){
-                    if(dfs(ngbr,destination))return true;
-                }
+    unordered_map<int,vector<int>>adjList;
+
+    bool dfs(int s, int d){
+        if(s==d)return true;
+        visited[s]=true;
+
+        for(auto &ngbr:adjList[s]){
+            if(visited[ngbr]==false){
+                if(dfs(ngbr,d))return true;
+                // visited[ngbr]=true;
             }
         }
         return false;
     }
+
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        this->n=n;
         visited.assign(n,false);
-        // i use a dfs or bfs from source if we get destination then true else false
-        // ii we can use DSU
-        
-        for(auto &edge:edges){
+        for(auto & edge:edges){
             int u=edge[0];
             int v=edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-
+            adjList[v].push_back(u);
+            adjList[u].push_back(v);
         }
-        
         return dfs(source,destination);
     }
 };
