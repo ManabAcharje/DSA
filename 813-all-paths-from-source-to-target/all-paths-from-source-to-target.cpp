a52dc442813d10184e7cf9f7d3809806aed5d651
Manab Acharjee <manavacharje@gmail.com>
2026-05-23 12:30:01 +0530

    Time: 2 ms (78.14%) | Memory: 15.5 MB (28.27%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<bool>visited;
+    int n;
+    void dfs(int node,vector<int>& temp,vector<vector<int>>& adj){
+        if(node==n-1){result.push_back(temp);return; }
+        visited[node]=true;
+        for(auto &ngbr: adj[node]){
+            if(!visited[ngbr]){
+                temp.push_back(ngbr);
+                visited[ngbr]=true;
+                dfs(ngbr,temp,adj);
+                temp.pop_back();
+                visited[ngbr]=false;
+            }
+
+
+        }
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adj) {
+        n=adj.size();
+        visited.resize(n,false);
+        vector<int>temp;
+        temp.push_back(0);
+        dfs(0,temp,adj);
+        return result;    
+    }
+};
