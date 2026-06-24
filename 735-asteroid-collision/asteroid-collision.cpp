ed47a3c29005518061fdcdd2e5819cea0b19c1bd
Manab Acharjee <manavacharje@gmail.com>
2026-06-24 21:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (89.41%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        vector<int> st;
+
+        for (int x : asteroids) {
+
+            bool alive = true;
+
+            while (alive &&
+                   !st.empty() &&
+                   st.back() > 0 &&
+                   x < 0) {
+
+                if (st.back() < -x) {
+                    st.pop_back();
+                }
+                else if (st.back() == -x) {
+                    st.pop_back();
+                    alive = false;
+                }
+                else {
+                    alive = false;
+                }
+            }
+
+            if (alive)
+                st.push_back(x);
+        }
+
+        return st;
+    }
+};
