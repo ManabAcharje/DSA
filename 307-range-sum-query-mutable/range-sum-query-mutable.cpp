78de857a269052024cb0d64ce369e1321ce74627
Manab Acharjee <manavacharje@gmail.com>
2026-06-25 12:09:15 +0530

    Time: 59 ms (62.91%) | Memory: 182.2 MB (52.66%) - LeetSync

---
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -0,0 +1,70 @@
+class NumArray {
+public:
+    int n;
+    vector<int> seg;
+
+    void build(int node, int l, int r, vector<int>& nums) {
+        if (l == r) {
+            seg[node] = nums[l];
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        build(2 * node + 1, l, mid, nums);
+        build(2 * node + 2, mid + 1, r, nums);
+
+        seg[node] = seg[2 * node + 1] + seg[2 * node + 2];
+    }
+
+    void updateSeg(int node, int l, int r, int idx, int val) {
+        if (l == r) {
+            seg[node] = val;
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        if (idx <= mid)
+            updateSeg(2 * node + 1, l, mid, idx, val);
+        else
+            updateSeg(2 * node + 2, mid + 1, r, idx, val);
+
+        seg[node] = seg[2 * node + 1] + seg[2 * node + 2];
+    }
+
+    int query(int node, int l, int r, int ql, int qr) {
+        if (qr < l || r < ql)
+            return 0;
+
+        if (ql <= l && r <= qr)
+            return seg[node];
+
+        int mid = l + (r - l) / 2;
+
+        return query(2 * node + 1, l, mid, ql, qr) +
+               query(2 * node + 2, mid + 1, r, ql, qr);
+    }
+
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        seg.resize(4 * n);
+
+        if (n > 0)
+            build(0, 0, n - 1, nums);
+    }
+
+    void update(int index, int val) {
+        updateSeg(0, 0, n - 1, index, val);
+    }
+
+    int sumRange(int left, int right) {
+        return query(0, 0, n - 1, left, right);
+    }
+};
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
