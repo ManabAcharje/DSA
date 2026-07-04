76bdcf4cb3d2cfc03b0fc2b73927e4eeb0214ec4
Manab Acharjee <manavacharje@gmail.com>
2026-07-04 12:16:57 +0530

    Time: 155 ms (10.53%) | Memory: 151.1 MB (9.41%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -5,17 +5,17 @@ public:
 
     int min_d;
 
-    void dfs(int node){
-        visited[node] = true;
-        for(auto &[v,wt]:adj[node]){
-            min_d = min (min_d, wt);
-            if(!visited[v]){
-                dfs(v);
+    // void dfs(int node){
+    //     visited[node] = true;
+    //     for(auto &[v,wt]:adj[node]){
+    //         min_d = min (min_d, wt);
+    //         if(!visited[v]){
+    //             dfs(v);
                 
-            }
+    //         }
 
-        }
-    }
+    //     }
+    // }
     int minScore(int n, vector<vector<int>>& roads) {
         //min paht 
         // it will be the the min weight of the edge in the component where node 1 is present
@@ -23,50 +23,50 @@ public:
         //so dfs on node one and track the min edge
         // unordered_map<int, vector<pair<int,int>>> adj;
 
-        // for(auto road: roads){
-        //     int u = road[0];
-        //     int v = road[1];
-        //     int wt = road[2];
-
-        //     adj[u].push_back({wt,v});
-        //     // adj[v].push_back({wt,u});
-
-            
-        // }
-        // //bfs(from node 1);
-        // visited.assign(n+1,false);
-        // queue<int> q;
-        // q.push(1);
-        // visited[1]= true;
-        // int min_d = INT_MAX;
-        // while(!q.empty()){
-        //     int u = q.front();
-        //     q.pop();
-        //     for(auto &[wt,v] : adj[u]){
-                
-        //         if(!visited[v]){
-        //             min_d = min(min_d,wt);
-        //             visited[v] = true;
-        //             q.push(v);
-        //         }
-
-        //     }
-           
-        // }
-        // return min_d;
-
-
-        for(auto & road : roads){
+        for(auto road: roads){
             int u = road[0];
             int v = road[1];
             int wt = road[2];
 
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+            adj[u].push_back({wt,v});
+            adj[v].push_back({wt,u});
+
+            
         }
+        //bfs(from node 1);
         visited.assign(n+1,false);
-        min_d = INT_MAX;
-        dfs(1);
+        queue<int> q;
+        q.push(1);
+        visited[1]= true;
+        int min_d = INT_MAX;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(auto &[wt,v] : adj[u]){
+                
+                min_d = min(min_d,wt);
+                if(!visited[v]){
+                    visited[v] = true;
+                    q.push(v);
+                }
+
+            }
+           
+        }
         return min_d;
+
+
+        // for(auto & road : roads){
+        //     int u = road[0];
+        //     int v = road[1];
+        //     int wt = road[2];
+
+        //     adj[u].push_back({v,wt});
+        //     adj[v].push_back({u,wt});
+        // }
+        // visited.assign(n+1,false);
+        // min_d = INT_MAX;
+        // dfs(1);
+        // return min_d;
     }
 };
