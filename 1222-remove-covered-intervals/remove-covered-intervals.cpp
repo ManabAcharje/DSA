e4266fd8169460833bade76f681a8518b470060f
Manab Acharjee <manavacharje@gmail.com>
2026-07-06 11:17:24 +0530

    Time: 3 ms (42.52%) | Memory: 15.2 MB (30.60%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-    // bool check(int l ,int r,vector<vector<int>> &covered_intervals){
-    //     for(auto &interval: covered_intervals){
-    //         int a = interval[0];
-    //         int b = interval[1];
-    //         if(l>=a && r<=b)return true;
-
-    //     }
-    //     return false;
-    // }
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        // remove intervals 
-        // i need to remove intervals that have been coved;
-        sort(intervals.begin(),intervals.end(),[](auto &a ,auto &b){
-            if(a[0] == b[0])return a[1]>b[1];
-            return a[0]<b[0];
-        });
 
-        int  n = intervals.size();
-        // vector<vector<int>>covered_intervals;
-        int removed= 0;
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 if (a[0] == b[0])
+                     return a[1] > b[1];
+                 return a[0] < b[0];
+             });
 
-        int maxEnd = -1;
-        for(auto &interval: intervals){
-            int l = interval[0];
-            int r = interval[1];
-            // if(check(l,r,covered_intervals))removed++;
-            // else{
-            //     covered_intervals.push_back({l,r});
-            // }
-            if(r<=maxEnd)removed++;
-            else{
-                maxEnd = r;
+        int removed = 0;
+        int maxRight = -1;
+
+        for (const auto &interval : intervals) {
+            if (interval[1] <= maxRight) {
+                removed++;
+            } else {
+                maxRight = interval[1];
             }
         }
-        return n - removed;
 
+        return intervals.size() - removed;
     }
 };
