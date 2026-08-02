ea4ad3ae3765836b767d6479b2e31c6bde399c71
Manab Acharjee <manavacharje@gmail.com>
2026-08-03 00:31:01 +0530

    Time: 19 ms (14.03%) | Memory: 19.8 MB (11.06%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+    int n ;
+    vector<int> nums;
+    vector<vector<int>>dp;
+    int solve(int i , int j){
+        if(i>j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int opt1  = nums[i]-solve(i+1,j);
+        int opt2 = nums[j]- solve(i,j-1);
+        return dp[i][j]=max(opt1, opt2);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        n = piles.size();
+        nums = piles;
+        dp.assign(n,vector<int>(n,-1));
+        int total = accumulate(piles.begin(),piles.end(),0);
+        return solve(0,n-1)>=0;
 
     }
 };
