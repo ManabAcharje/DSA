c5a023a8a31aed3c82e7a146606982092fc35fe0
Manab Acharjee <manavacharje@gmail.com>
2026-09-06 13:09:19 +0530

    Time: 451 ms (8.41%) | Memory: 141.3 MB (8.34%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string s, t;
     int m, n;
-    long long dp[1001][1001];
+    unordered_map<long long ,int>memo;
 
     long long solve(int i, int j) {
         if (j == m)
@@ -10,15 +10,18 @@ public:
 
         if (i == n)
             return 0;
+        
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
+        long long  key = ((long long)i<<32)|j;
+
+        if (memo.count(key))
+            return memo[key];
 
         if (s[i] == t[j]) {
-            return dp[i][j] = solve(i + 1, j + 1) + solve(i + 1, j);
+            return memo[key] = solve(i + 1, j + 1) + solve(i + 1, j);
         }
 
-        return dp[i][j] = solve(i + 1, j);
+        return memo[key] = solve(i + 1, j);
     }
 
     int numDistinct(string s, string t) {
@@ -28,7 +31,7 @@ public:
         n = s.size();
         m = t.size();
 
-        memset(dp, -1, sizeof(dp));
+        
 
         if (n < m)
             return 0;
